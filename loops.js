bf9d197e212d06cc2d2b5c748522a41854904d67
RoyKersten <roykersten@icloud.com>
2021-10-09 10:59:04 +0200

    excercise loops finalized

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,9 +18,12 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
+for (let i = 0; i < 4; i++) {
+    console.log("Loop...")
+}
+console.log("Klaar!")
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -33,6 +38,13 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even")
+    } else if (i % 2 !== 0) {
+        console.log(i + " is oneven")
+    }
+}
 
 
 // ==========================================
@@ -50,4 +62,14 @@
 // >> 8
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i)
+    } else if (i >= 3 && i < 6) {
+        console.log("> " + i)
+    } else if (i >= 6 && i < 10) {
+        console.log(">> " + i)
+    }
+}
 
